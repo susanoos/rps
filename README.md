1abe8341fa464229a447cd0ced01515bb3b14acc
David <escxbar@gmail.com>
2022-02-18 17:58:22 -0800

    Finished project needs styling

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# rps
+1. Add function for computer to chose rock, paper, scissors
+2. Prompt user to select choice
+3. Add function to play a round of the game
+4. Add function to play 5 rounds
--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-let playerSelection, computerSelection, computerScore, playerScore;
-const gameScore = [];
+let computerScore, playerScore;
 const choice = ["rock", "paper", "scissors"];
+const playerPoints = document.querySelector(".player-score");
+const computerPoints = document.querySelector(".computer-score");
+const btn = document.querySelectorAll("input");
+
+const disableBtns = () => {
+  btn.forEach((el) => {
+    el.disabled = true;
+  });
+};
 
 const computerPlay = () => {
   const random = Math.floor(Math.random() * choice.length);
   return choice[random];
 };
 
-computerSelection = computerPlay();
-playerSelection = prompt("rock paper scissors");
 const playRound = (playerSelection) => {
+  let result = "";
+  const computerSelection = computerPlay();
+  playerScore = playerPoints.innerHTML;
+  computerScore = computerPoints.innerHTML;
   if (playerSelection === computerSelection) {
     console.log(`Tied`);
   } else if (
@@ -17,24 +27,47 @@ const playRound = (playerSelection) => {
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
+    playerScore++;
+    playerPoints.innerHTML = playerScore;
     console.log(`Player wins!`);
     console.log(`Player scored a point`);
+    if (playerScore === 5) {
+      console.log(`Player won`);
+      document.querySelector(".player-winner-text").innerHTML =
+        "Player won the 5 rounds!";
+      addEle();
+      disableBtns();
+    }
   } else if (
     (computerSelection === "rock" && playerSelection === "scissors") ||
     (computerSelection === "paper" && playerSelection === "rock") ||
     (computerSelection === "scissors" && playerSelection === "paper")
   ) {
+    computerScore++;
+    computerPoints.innerHTML = computerScore;
     console.log(`Computer wins`);
     console.log(`Computer scored a point`);
-  }
-};
-// console.log(playerSelection);
-// console.log(computerSelection);
-// playRound(playerSelection);
 
-const game = () => {
-  for (let i = 0; i < 5; i++) {
-    playRound(playerSelection);
+    if (computerScore === 5) {
+      console.log(`Computer won`);
+      document.querySelector(".computer-winner-text").innerHTML =
+        "Computer won the 5 rounds!";
+      addEle();
+      disableBtns();
+    }
   }
 };
-game();
+
+btn.forEach((btns) => {
+  btns.addEventListener("click", () => {
+    playRound(btns.value);
+  });
+});
+
+const addEle = () => {
+  const newPara = document.createElement("p");
+  const newParaText = document.createTextNode("Reload page to play again");
+  newPara.appendChild(newParaText);
+  const currentDiv = document.querySelector(".game-score");
+  document.body.insertBefore(newPara, currentDiv);
+};
